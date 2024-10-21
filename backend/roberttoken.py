843c81f678636d93e8119c0aa89a32026d85b2bd
Anshi <anshigandhi2002@gmail.com>
2024-10-22 00:19:23 +0530

    hi

--- backend/roberttoken.py
+++ backend/roberttoken.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 from transformers import RobertaTokenizer
 
 # Load dataset from jsonl
-dataset = load_dataset('json', data_files='/media/anshi/OS/Users/anshi/Documents/project/Clone-detection-POJ-104/dataset/train.jsonl')
+dataset = load_dataset('json', data_files='train.jsonl')
 
 # Load tokenizer (CodeBERT or GraphCodeBERT)
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
@@ -12,3 +12,5 @@ def tokenize_code(example):
     return tokenizer(example['code'], padding='max_length', truncation=True, max_length=5000)
 
 tokenized_dataset = dataset.map(tokenize_code, batched=True)
+
+print(tokenized_dataset['train'])
